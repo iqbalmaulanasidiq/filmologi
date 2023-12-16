b2bf832dbe85b0ac63ca8290ce2dabc1ecd09ec0
Vina Nur Fauziah <@vinaanurfauziah@gmail.com>
2023-12-16 20:15:13 +0700

    menambahkan bar search pada index dashboard admin

--- resources/views/dashboard-adm/index.blade.php
+++ resources/views/dashboard-adm/index.blade.php
@@ -31,6 +31,20 @@
         <div class="flex items-center justify-center h-20 rounded bg-white-50 dark:bg-white-800">           
         </div>
 
+        {{-- SEARCH --}}
+        <form>   
+            <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+            <div class="relative">
+                <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                    </svg>
+                </div>
+                <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search ..." required>
+            </div>
+        </form>
+       </div>
+
        {{-- TULISAN DASHBOARD ADMIN --}}
        <div class="flex items-center justify-left h-1 mb-4 rounded bg-white-50 dark:bg-white-800">
           <p class="text-lg font-medium text-gray-500 dark:text-gray-400">Dashboard Admin</p>

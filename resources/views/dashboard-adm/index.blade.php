3ae4f364b5868413f3415282b347ca2a7fc09d73
Vina Nur Fauziah <@vinaanurfauziah@gmail.com>
2023-12-16 20:12:15 +0700

    menambahkan breadcrumb pada halaman index dashboard admin

--- resources/views/dashboard-adm/index.blade.php
+++ resources/views/dashboard-adm/index.blade.php
@@ -6,6 +6,31 @@
     <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
        <div class="grid grid-cols-3 gap-4 mb-4">
 
+        {{-- BREADCRUMB --}}
+        <nav class="flex" aria-label="Breadcrumb">
+            <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+            <li class="inline-flex items-center">
+                <a href="#" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-gray-800">
+                <svg class="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                    <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"/>
+                </svg>
+                Home
+                </a>
+            </li>
+            <li aria-current="page">
+                <div class="flex items-center">
+                <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                </svg>
+                <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">Dashboard Admin</span>
+                </div>
+            </li>
+            </ol>
+        </nav>
+        
+        <div class="flex items-center justify-center h-20 rounded bg-white-50 dark:bg-white-800">           
+        </div>
+
        {{-- TULISAN DASHBOARD ADMIN --}}
        <div class="flex items-center justify-left h-1 mb-4 rounded bg-white-50 dark:bg-white-800">
           <p class="text-lg font-medium text-gray-500 dark:text-gray-400">Dashboard Admin</p>

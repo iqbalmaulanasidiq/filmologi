8b5d3ed737f4d6b07e268f5a356fbc081e688d87
Vina Nur Fauziah <@vinaanurfauziah@gmail.com>
2023-12-19 07:13:45 +0700

    bikin folder&route users & role, perbaiki beberapa bagian di home dashboard admin

--- routes/web.php
+++ routes/web.php
@@ -51,3 +51,11 @@ require __DIR__.'/auth.php';
 Route::get('/dashboard-adm', function () {
     return view('dashboard-adm.index');
 })->middleware(['auth', 'verified'])->name('admin');
+
+Route::get('/dashboard-adm/users/index', function () {
+    return view('dashboard-adm.users.index');
+})->middleware(['auth', 'verified'])->name('dashboard-adm.users.index');
+
+Route::get('/dashboard-adm/role/index', function () {
+    return view('dashboard-adm.role.index');
+})->middleware(['auth', 'verified'])->name('dashboard-adm.role.index');

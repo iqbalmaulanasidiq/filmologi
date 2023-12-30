9902cc32d33d247280b901d38879cc1d9859be43
iqbalmaulanasidiq <iqbal.213040061@mail.unpas.ac.id>
2023-12-30 14:00:23 +0700

    migrasi lupa

--- database/migrations/2023_12_29_165112_add_profile_image_to_users_table.php
+++ database/migrations/2023_12_29_165112_add_profile_image_to_users_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
+            $table->text('profile_image')->nullable();
             $table->string('github_id')->nullable();
             $table->string('auth_type')->nullable();
         });
